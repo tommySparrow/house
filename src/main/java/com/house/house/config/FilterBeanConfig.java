204dd965a15d2d2b8a7faecca0012dda4df8e04f
jerry <jm90_yang@163.com>
2018-10-24 17:25:05 +0800

    统一管理config文件

--- src/main/java/com/house/house/config/FilterBeanConfig.java
+++ src/main/java/com/house/house/config/FilterBeanConfig.java
@@ -1,5 +1,6 @@
-package com.house.house.filter;
+package com.house.house.config;
 
+import com.house.house.filter.LogFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/resources/sql/record.sql
+++ src/main/resources/sql/record.sql
@@ -1,3 +1,5 @@
+-- noinspection SqlNoDataSourceInspectionForFile
+
 /*
  Navicat Premium Data Transfer
 
