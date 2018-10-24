6fbeeb60ba74461e0c441963397ddffc51f4440d
jm90_yang <jm90_yang@163.com>
2018-10-24 22:02:31 +0800

    整合freemarker,注意不要使用@RestController(会使返回地址失效当做字符串返回)

---
+++ src/main/java/com/house/house/controller/FreemarkerController.java
@@ -0,0 +1,27 @@
+package com.house.house.controller;
+
+import com.house.house.bean.Record;
+import com.house.house.service.RecordService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+public class FreemarkerController {
+
+
+    @Autowired
+    private RecordService recordService;
+
+    @RequestMapping("/test")
+    public String testRecords(ModelMap modelMap){
+
+        List<Record> recordList = recordService.selectRecords();
+        Record record = recordList.get(0);
+        modelMap.addAttribute("record",record);
+        return "hello";
+    }
+}
