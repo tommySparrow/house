913741143d445796148e3350170ff8308e83290e
jerry <jm90_yang@163.com>
2018-10-25 13:55:31 +0800

    回退到spring boot模式启动

--- house-web/src/main/java/com/house/house/HouseApplication.java
+++ house-web/src/main/java/com/house/house/HouseApplication.java
@@ -2,16 +2,9 @@ package com.house.house;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HouseApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(HouseApplication.class);
-    }
+public class HouseApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HouseApplication.class, args);
