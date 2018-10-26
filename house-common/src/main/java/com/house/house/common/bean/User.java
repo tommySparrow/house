1fbdacacf3a03b20eb770c5ea20f4382671b77ef
jerry <jm90_yang@163.com>
2018-10-26 10:04:51 +0800

    注释static page and user basic ath

---
+++ house-common/src/main/java/com/house/house/common/bean/User.java
@@ -0,0 +1,70 @@
+package com.house.house.common.bean;
+
+/**
+ * @ Author     ：jmyang
+ * @ Date       ：Created in 2018/10/26
+ * @ Description：User bean
+ * @ throws
+ */
+public class User {
+
+    private Long id;
+
+    private String email;
+
+    private String phone;
+
+    private String name;
+
+    private String passwd;
+
+    private String confirmPasswd;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPasswd() {
+        return passwd;
+    }
+
+    public void setPasswd(String passwd) {
+        this.passwd = passwd;
+    }
+
+    public String getConfirmPasswd() {
+        return confirmPasswd;
+    }
+
+    public void setConfirmPasswd(String confirmPasswd) {
+        this.confirmPasswd = confirmPasswd;
+    }
+}
---
+++ house-web/src/main/java/com/house/house/web/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.house.house.web.controller;
+
+import com.house.house.common.bean.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @ Author     ：jmyang
+ * @ Date       ：Created in 2018/10/26
+ * @ Description：register user logic of basic
+ * @ throws
+ */
+@Controller
+public class UserController {
+
+    /**
+     * @ Author : jmyang
+     * validate params
+     */
+    @RequestMapping("/accounts/register")
+    public String accountsRegister(User accont, ModelMap modelMap){
+
+        if (null == accont || null == accont.getName()){
+
+            return "/user/accounts/register";
+        }
+        return null;
+    }
+}
