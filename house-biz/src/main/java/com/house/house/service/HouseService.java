39ba4601e7671108a9109827336cccd02f967e1b
jerry <jm90_yang@163.com>
2018-10-31 11:32:39 +0800

    添加静态资源页面

---
+++ house-biz/src/main/java/com/house/house/service/HouseService.java
@@ -0,0 +1,14 @@
+package com.house.house.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @ Author     ：jmyang
+ * @ Date       ：Created in 2018/10/31
+ * @ Description：
+ * @ throws
+ */
+@Service
+public class HouseService {
+
+}
---
+++ house-web/src/main/java/com/house/house/web/controller/HouseController.java
@@ -0,0 +1,37 @@
+package com.house.house.web.controller;
+
+import com.house.house.service.HouseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @ Author     ：jmyang
+ * @ Date       ：Created in 2018/10/31
+ * @ Description：
+ * @ throws
+ */
+@Controller
+public class HouseController {
+
+    @Autowired
+    private HouseService houseService;
+
+    /**
+     * @ Author jmy
+     * @ Description //TODO User
+     * @ Date 2018/10/31
+     * @ Param []
+     * @ return java.lang.String
+     * 1.实现分页
+     * 2.支持小区搜索、类型搜索
+     * 3.支持排序
+     * 4.支持展示图片、价格、标题、地址等信息
+     **/
+    @RequestMapping("/house/list")
+    public String houseList(){
+
+        return "house/listing";
+    }
+
+}
